09d69f486694f265a12b14231163f5450f56d0c7
L <lorenzomrdev@gmail.com>
2025-08-03 21:58:13 -0400

    First Commit

---
+++ main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <cassert>
+
+//Maybe change some of these to uint_fast32_t || int_fast32_t respectively
+//Point of all this code is to save an img to the disk
+
+uint32_t pack_color(const uint8_t r, const uint8_t b, const uint8_t g, const uint8_t a=255) {
+    return (a<<24) + (b<<16) + (g<<8) + r;
+}
+
+void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &b, uint8_t &g, uint8_t &a) {
+    r = (color >> 0) & 255;
+    g = (color >> 8) & 255;
+    b = (color >> 16) & 255;
+    a = (color >> 24) & 255;
+}
+
+void drop_ppm_image(const std::string filename, const std::vector <uint32_t> &image, const size_t height, const size_t width) {
+    assert(image.size() == width*height); //Makes sure img is product of screen height & width
+    std::ofstream ofs;
+    ofs.open(filename,std::ofstream::out | std::ofstream::binary); //Issue with windows GCC
+
+    if (!ofs) { std::cout << "Error opening file" << std::endl;
+        return;}
+    ofs << "P6\n" << width << " "<<  height << "\n255\n"; //Bug here maybe
+    for (size_t i = 0; i < height*width; ++i) {
+        uint8_t r,g,b,a;
+        unpack_color(image[i],r,g,b,a);
+        ofs << static_cast<char>(r) << static_cast<char>(g) << static_cast<char>(b);
+    }
+
+    ofs.close();
+}
+
+void draw_rectangle(std::vector<uint32_t> &img, const size_t img_w, const size_t &img_h, const size_t x, const size_t y, const size_t w, const size_t h, const uint32_t color) {
+    assert(img.size() == img_w*img_h);
+
+    for (size_t i = 0; i < w; i++) {
+        for (size_t j = 0; j < h; j++) {
+            size_t cx = x+i;
+            size_t cy = y+j;
+            assert(cx < img_w && cy < img_h);
+            img[cx + cy*img_w] = color;
+        }
+    }
+}
+
+int main() {
+    const size_t win_w = 512; //Image width
+    const size_t win_h = 512; //Image height
+    std::vector<uint32_t> framebuffer(win_w*win_h, 255);//Image just plain red
+
+    //------------Map------------//
+    const size_t map_w = 16;
+    const size_t map_h = 16;
+    const char map[] = "0000222222220000"\
+                   "1              0"\
+                   "1      11111   0"\
+                   "1     0        0"\
+                   "0     0  1110000"\
+                   "0     3        0"\
+                   "0   10000      0"\
+                   "0   0   11100  0"\
+                   "0   0   0      0"\
+                   "0   0   1  00000"\
+                   "0       1      0"\
+                   "2       1      0"\
+                   "0       0      0"\
+                   "0 0000000      0"\
+                   "0              0"\
+                   "0002222222200000";
+    assert(sizeof(map) == map_w*map_h+1);
+
+    for (size_t j = 0; j < win_h; j++) { //This loop fills the screen with color
+        for (size_t i = 0; i < win_w; i++) {
+            uint8_t r = 255*j/float(win_h); //Varies 0 - 255 as j sweeps vertically
+            uint8_t g = 255*i/float(win_w); // Varies 0 - 255 as j sweeps horizontally
+            uint8_t b = 0;
+            framebuffer[i+j*win_w] = pack_color(r,g,b);
+        }
+    }
+    const size_t rect_w = win_w/map_w;
+    const size_t rect_h = win_h/map_h;
+
+    //-----------Draw map----------//
+    for (size_t j = 0; j < map_h; j++) {
+        for (size_t i = 0; i < map_w; i++) {
+            if (map[i+j*map_w]==' ') continue; //Skips empty spaces
+            size_t rect_x = i*rect_w;
+            size_t rect_y = j*rect_h;
+            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h,pack_color(0, 255, 255));
+        }
+    }
+    //-----------------------------//
+
+
+
+    drop_ppm_image("./image.ppm",framebuffer,win_w, win_h);
+    return 0;
+}
